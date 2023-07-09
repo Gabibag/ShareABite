130b867c70166392e4b8139af714582d278544ae
Gabibag <97263038+Gabibag@users.noreply.github.com>
2023-07-08 21:08:57 -0700

    setup

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.post('/api/getDatabase')
+def getDatabase():
+    return "Hello World"
+
+
+if __name__ == '__main__':
+    app.run()
