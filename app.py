0ce3b52df57929b4c5fd01d1e8306335b1144957
Gabibag <97263038+Gabibag@users.noreply.github.com>
2023-07-22 12:53:14 -0700

    front pages rework (fun)

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import dotenv
 def index():
     return render_template('/pages/homepage.html')
 
+
 @app.route('/<path:url>', methods=['get'])
 def sendPage(url):
     # check to see if the file exists, if not, return 404
@@ -21,20 +22,25 @@ def sendPage(url):
         return "404"
     return render_template('/pages/' + url + '.html')
 
+
 @app.route('/api/getFoodTypes', methods=['get'])
 def getFoodTypes():
     # get food types from database
     return [org['food'] for org in
             sqlite3.connect('database.db').cursor().execute("SELECT * FROM organizations").fetchall()]
 
+
 @app.route("/image/<path:url>", methods=['get'])
 def returnImage(url):
     print("aaaa")
     return send_file("static/Image/" + url, mimetype='image/gif')
 
+
 @app.route("/script/<path:url>", methods=['get'])
 def returnJs(url):
     return send_file("templates/scripts/" + url)
+
+
 @app.route("/style/<path:url>", methods=['get'])
 def returnCSS(url):
     return send_file("templates/styles/" + url)
@@ -56,14 +62,14 @@ def getDatabase():
             st += str(org[0]) + "\n"
     if len(orgs) == 0:
         return "No organizations found"
-    
+
     try:
         orgsdist = []
         for org in orgs:
             get = getClosestOrg(local, org[1]).get('duration').get('text')
             print(get)
             orgsdist.append({org[0], get})
-        
+
         nums = ["1,", "2,", "3,", "4,", "5,", "6,", "7,", "8,", "9,", "0,"]
         for org in orgsdist:
             str1 = list(org)[0]
@@ -97,6 +103,7 @@ def saveInfo():
 
     return "saved"
 
+
 @app.post('/api/imageRec')
 def imageRec():
     file = request.files['file']
@@ -111,8 +118,9 @@ def imageRec():
         messages=[
             {"role": "user",
              "content": "In this sentence pick out the name of the every main food item: "
-                        + str(food) + ". Only return the name of the food items and nothing else. for example 'beef stew', "
-                        "'carrots', 'spinach'"}
+                        + str(
+                 food) + ". Only return the name of the food items and nothing else. for example 'beef stew', "
+                         "'carrots', 'spinach'"}
         ]
     )
     return completion.choices[0].message.content.split(",")
